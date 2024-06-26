ac7ccb8bf4dab83886825147e02d59bffdcfe341
Chayud-Ac <acieacie@hotmail.com>
2024-06-26 17:52:54 +0700

    Created header section in the Home component Page

---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Once you try you will forget your name</h2>
+        <p>
+          Choose from a menu featuring a delectable array of dishes crafted with
+          the finest satisfy your craving and elevate your dining experience ,
+          one delicious meal at a time
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
