0295185727a76ba0c224b284d48da588c0d47962
Chayud-Ac <acieacie@hotmail.com>
2024-06-27 01:06:41 +0700

    AppDownload component added

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.css";
 import Header from "../../Components/Header/Header";
 import ExploreMenu from "../../Components/ExploreMenu/ExploreMenu";
 import FoodDisplay from "../../Components/FoodDisplay/FoodDisplay";
+import AppDownload from "../../Components/AppDownload/AppDownload";
 
 const Home = () => {
   //   this state category variable is used to track the current category base on the onClick element from the user
@@ -13,6 +14,7 @@ const Home = () => {
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category} />
+      <AppDownload />
     </div>
   );
 };
