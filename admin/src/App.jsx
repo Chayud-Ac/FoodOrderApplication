fb2672a56b5ca05c141fc4d1346f13ad8242ef04
Chayud-Ac <acieacie@hotmail.com>
2024-06-29 01:19:27 +0700

    Add pages including Add page , List page , Orders page. Also adding the Routes and styling the active route

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Navbar from "./Components/Narbar/Navbar";
 import Sidebar from "./Components/Sidebar/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./Components/pages/Add/Add";
+import List from "./Components/pages/List/List";
+import Orders from "./Components/pages/Orders/Orders";
 
 const App = () => {
   return (
@@ -9,6 +13,11 @@ const App = () => {
       <hr />
       <div className="app-content">
         <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
       </div>
     </div>
   );
--- admin/src/Components/Sidebar/Sidebar.jsx
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import "./Sidebar.css";
 import { assets } from "../../assets/assets";
+import { NavLink } from "react-router-dom";
+
+// The different between Link and NavLink is that NavLink automatically add className props to the active route that user click 'active'
+// we can use active className to style the active route without manually creating the active state variable
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar-options">
-        <div className="sidebar-option">
+        <NavLink to="/add" className="sidebar-option">
           <img src={assets.add_icon} alt="" />
           <p>Add Items</p>
-        </div>
-        <div className="sidebar-option">
+        </NavLink>
+        <NavLink to="/list" className="sidebar-option">
           <img src={assets.order_icon} alt="" />
           <p>List Item</p>
-        </div>
-        <div className="sidebar-option">
+        </NavLink>
+        <NavLink to="/orders" className="sidebar-option">
           <img src={assets.order_icon} alt="" />
           <p>Orders</p>
-        </div>
+        </NavLink>
       </div>
     </div>
   );
