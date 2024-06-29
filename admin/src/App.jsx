34634586b19a72e7c3781f852e21ce0b99dff3d0
Chayud-Ac <acieacie@hotmail.com>
2024-06-29 19:23:15 +0700

    created API call from the Add page to the backend api end point api/food/add for adding foodItem in the database

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,10 +5,13 @@ import { Route, Routes } from "react-router-dom";
 import Add from "./Components/pages/Add/Add";
 import List from "./Components/pages/List/List";
 import Orders from "./Components/pages/Orders/Orders";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <div>
+      <ToastContainer />
       <Navbar />
       <hr />
       <div className="app-content">
