60e7306508584e5dda2e9ba5e37d4051e693e043
Chayud-Ac <acieacie@hotmail.com>
2024-07-02 01:27:50 +0700

    created status route api endpoint in backend for updating the status of the orderItem when admin panel send an updated status

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -5,6 +5,7 @@ import {
   verifyOrder,
   userOrders,
   listOrders,
+  updateStatus,
 } from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
@@ -15,5 +16,6 @@ orderRouter.post("/place", authMiddleware, placeOrder);
 orderRouter.post("/verify", verifyOrder);
 orderRouter.get("/userorders", authMiddleware, userOrders);
 orderRouter.get("/list", listOrders);
+orderRouter.post("/status", updateStatus);
 
 export { orderRouter };
