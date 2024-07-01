fdda2d64539a5cd0741b2785fdb978862f133d67
Chayud-Ac <acieacie@hotmail.com>
2024-07-02 00:28:19 +0700

    adding userorders api endpoint for response the order data to allow frontend user to track the orderData and status

---
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -0,0 +1,56 @@
+import React, { useContext, useEffect, useState } from "react";
+import "./MyOrders.css";
+import { StoreContext } from "../../context/StoreContext";
+import axios from "axios";
+import { assets } from "../../assets/assets";
+
+const MyOrders = () => {
+  const [data, setData] = useState([]);
+  const { url, token } = useContext(StoreContext);
+
+  const fetchOrders = async () => {
+    const response = await axios.get(url + "/api/order/userorders", {
+      headers: { token },
+    });
+
+    setData(response.data.data);
+  };
+
+  useEffect(() => {
+    if (token) {
+      fetchOrders();
+    }
+  }, [token]);
+
+  return (
+    <div className="my-orders">
+      <h2>My Orders</h2>
+      <div className="container">
+        {data.map((order) => (
+          <div key={order._id} className="my-orders-order">
+            <img src={assets.parcel_icon} alt="" />
+            <p>
+              {order.items.map((item, index) => {
+                if (index === order.items.length - 1) {
+                  // index === order.item.length - 1 // when index is equal to last index don't display ","
+                  return item.name + " x " + item.quantity;
+                } else {
+                  return item.name + " x " + item.quantity + ",";
+                }
+              })}
+            </p>
+            <p>${order.amount}.00</p>
+            <p>Total Items : {order.items.length}</p>
+            <p>
+              <span>&#x25cf; </span>
+              <b>{order.status}</b>
+            </p>
+            <button>Track Order</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MyOrders;
