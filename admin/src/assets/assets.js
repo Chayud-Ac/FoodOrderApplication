b3436fa66a0281a086d6d2ff09d5e2246b784c01
Chayud-Ac <acieacie@hotmail.com>
2024-07-03 20:50:41 +0700

    Changes admin page style

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,17 +1,19 @@
-import logo from './logo.png'
-import add_icon from './add_icon.png'
-import order_icon from './order_icon.png'
-import profile_image from './profile_image.png'
-import upload_area from './upload_area.png'
-import parcel_icon from './parcel_icon.png'
+import logo from "./logo.png";
+import add_icon from "./add_icon.png";
+import order_icon from "./order_icon.png";
+import profile_icon from "./profile_icon.svg";
+import upload_area from "./upload_area.png";
+import parcel_icon from "./parcel_icon.png";
+import GrabPanda_logo from "./GrabPanda_logo.svg";
 
-export const assets ={
-    logo,
-    add_icon,
-    order_icon,
-    profile_image,
-    upload_area,
-    parcel_icon
-}
+export const assets = {
+  logo,
+  add_icon,
+  order_icon,
+  profile_icon,
+  upload_area,
+  parcel_icon,
+  GrabPanda_logo,
+};
 
-export const url = 'http://localhost:4000'
+export const url = "http://localhost:4000";
