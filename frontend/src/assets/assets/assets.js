9e11ae087d3d88e987742253fa8ca84d9cf7a539
Chayud-Ac <acieacie@hotmail.com>
2024-07-16 17:50:00 +0700

    Edited Icon

---
+++ frontend/src/assets/assets/assets.js
@@ -0,0 +1,19 @@
+import logo from "./logo.png";
+import add_icon from "./add_icon.png";
+import order_icon from "./order_icon.png";
+import profile_icon from "./profile_icon.svg";
+import upload_area from "./upload_area.png";
+import parcel_icon from "./parcel_icon.png";
+import GrabPanda_logo from "./GrabPanda_logo.svg";
+
+export const assets = {
+  logo,
+  add_icon,
+  order_icon,
+  profile_icon,
+  upload_area,
+  parcel_icon,
+  GrabPanda_logo,
+};
+
+export const url = "http://localhost:4000";
